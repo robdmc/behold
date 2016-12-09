73e409f66f33805e5ac3e37620da19a0bf6d85ec
Rob deCarvalho <robdmc@gmail.com>
2016-12-09 17:54:37 -0500

    working on getting logger going

--- behold/logger.py
+++ behold/logger.py
@@ -52,8 +52,10 @@ https://gist.github.com/pombredanne/72130ee6f202e89c13bb
 """
 
 class Behold(object):
+    # class variable to hold all context values
     _context = {}
 
+    # operators to handle django-style querying
     _op_for = {
         '__lt': operator.lt,
         '__lte': operator.le,
@@ -66,14 +68,19 @@ class Behold(object):
     }
 
     def __init__(self, item=None, tag=None):
+        """
+        If item is None, then locals() + globals() is used.
+        If its not None, then it must either be a dict or
+        something with a dict attribute
+        """
+        # if no item, get locals() and globals() from calling frame 
         if item is None:
-            frame = inspect.currentframe()
-            try:
-                item = frame.f_back.f_locals
-                item.update(frame.f_back.f_globals)
-            finally:
-                del frame
+            self.frame = inspect.currentframe().f_back
+            item = self._load_frame()
+        else:
+            self.frame = None
 
+        # if the item is not a dictionary, grab its object __dict__
         if not isinstance(item, dict):
             try:
                 item = item.__dict__
@@ -83,14 +90,41 @@ class Behold(object):
                     'dicts or that have a .__dict__ attribute.'
                 )
 
+        # sore the item to work with as well as any tag for this beholder
         self.item = item
         self.tag = tag
+
+        # these filters will apply to item or scope variables
         self.all_filters = []
         self.any_filters = []
+
+        # these filters apply to context variables
         self.all_context_filters = []
         self.any_context_filters = []
 
+        # a list of fields that will be printed if filters pass
+        self.print_keys = []
+
+    def _load_frame(self):
+        OKAY.  HERE IS WHAT IM DOING.  IM PRETY SURE LOCAL/GLOBALS HAVE TO GET
+        RELOADED EVERYTIME A BOOL IS CHECKED. SO IM TRYING TO GET THAT TO HAPPEN
+        HERE, BUT ITS TRICKY TO GET A FRAME REFERENCE AND DELETE IT PROPERLY
+        MAYBE I CAN SAVE THE FRAME DELETION FOR A DESTRUCTOR
+            try:
+                item = self.frame.f_back.f_locals
+                item.update(self.frame.f_back.f_globals)
+            finally:
+                se
+                del self.frame
+                self
+
+            return item
+
     def _key_to_field_op(self, key):
+        # this method looks at a key and checks if it ends in any of the
+        # endings that have special django-like query meanings.
+        # It translates those into comparision operators and returns the
+        # name of the actual key.
         op = operator.eq
         name = key
         for op_name, trial_op in self.__class__._op_for.items():
@@ -102,15 +136,24 @@ class Behold(object):
 
     @classmethod
     def set_context(cls, **kwargs):
+        """
+        This method sets context variables
+        """
         cls._context.update(kwargs)
 
     @classmethod
     def unset_context(cls, *keys):
+        """
+        This method unsets the specified context variables
+        """
         for key in keys:
             if key in cls._context:
                 cls._context.pop(key)
 
     def _add_filters(self, exclude, in_context, join_with, **criteria):
+        # This is the method does all the work for creating a filter
+        # list that will then be used to determine whether or not
+        # this beholder passes filters.
 
         #TODO: make sure you test all 4 combos
         # keyed on (in_context, join_with)
@@ -127,6 +170,9 @@ class Behold(object):
 
     def _passes_one_filter(
             self, filter_list, is_context=False, is_any_filter=False):
+        # This method does all the work for using the built-up state to
+        # determine whether or not this beholder passes filters.
+
         # set the item you are comparing to based on whether or not this is
         # a context comparison
         if is_context:
@@ -134,19 +180,25 @@ class Behold(object):
         else:
             item = self.item
 
+        if not filter_list:
+            return True
+
         passes = not is_any_filter
-        for (op, field, filter_val, exclude) in self._filters:
+        for (op, field, filter_val, exclude) in filter_list:
             # do not use the extractor for context variables
             if is_context:
                 field_val = item[field]
             else:
                 field_val = self.extract(item, field)
-            passes = exclude ^ op(field_val, val)
+            passes = exclude ^ op(field_val, filter_val)
             if (not is_any_filter) ^ passes:
                 break
         return passes
 
     def _passes_filter(self):
+        # This is the high-level method that determines whether or not
+        # this beholder passes filters.
+
         # tuples are (filter_list, is_context, is_any_filter)
         filter_tuples = [
             (self.all_filters, False, False),
@@ -155,70 +207,136 @@ class Behold(object):
             (self.any_context_filters, True, True),
         ]
 
+        print '=--'
         passes = True
         for filter_list, is_context, is_any_filter in filter_tuples:
+            print passes
             passes = self._passes_one_filter(filter_list, is_context, is_any_filter)
             if not passes:
                 break
+        print passes, filter_list
         return passes
 
     def when_all(self, **criteria):
+        """
+        Beholder will evaluate to True only if all criteria are met
+        """
         self._add_filters(
             exclude=False, in_context=False, join_with='all', **criteria)
         return self
 
     def when_any(self, **criteria):
+        """
+        Beholder will evaluate to True if any of the criteria are met
+        """
         self._add_filters(
             exclude=False, in_context=False, join_with='any', **criteria)
         return self
 
-    def exlude_when_any(self, **criteria):
-        self._add_filters(
-            exclude=True, in_context=False, join_with='any', **criteria)
-        return self
-
     def exlude_when_all(self, **criteria):
+        """
+        Beholder will evaluate to False if all of the criteria are met
+        """
         self._add_filters(
             exclude=True, in_context=False, join_with='all', **criteria)
         return self
 
+    def exlude_when_any(self, **criteria):
+        """
+        Beholder will evaluate to False if any of the criteria are met
+        """
+        self._add_filters(
+            exclude=True, in_context=False, join_with='any', **criteria)
+        return self
+
+
     def when_all_in_context(self, **criteria):
+        """
+        Beholder will evaluate to True if all context criteria are met
+        """
         self._add_filters(
             exclude=False, in_context=True, join_with='all', **criteria)
         return self
 
     def when_any_in_context(self, **criteria):
+        """
+        Beholder will evaluate to True if any context criteria are met
+        """
         self._add_filters(
             exclude=False, in_context=True, join_with='any', **criteria)
         return self
 
-    def exlude_when_any_in_context(self, **criteria):
-        self._add_filters(
-            exclude=True, in_context=True, join_with='any', **criteria)
-        return self
-
     def exlude_when_all_in_context(self, **criteria):
+        """
+        Beholder will evaluate to False if all context criteria are met
+        """
         self._add_filters(
             exclude=True, in_context=True, join_with='all', **criteria)
         return self
 
+    def exlude_when_any_in_context(self, **criteria):
+        """
+        Beholder will evaluate to False if any context criteria are met
+        """
+        self._add_filters(
+            exclude=True, in_context=True, join_with='any', **criteria)
+        return self
+
+
     def values(self, *fields):
+        """
+        Sets the attributes or local variables that will be returned.
+        If no arguments are specified, all attributes/variables get shown
+        in key-sorted order.
+        """
+
         return self
 
     @classmethod
     def load_global_state(cls):
         """
-        Used for loading any state needed for transform method
+        Used for loading any state needed for the extract method
         """
 
     def extract(self, item, name):
-        return self.item[name]
+        """
+        Override this to perform any custom field extraction
+        """
+        val = self.item.get(name, None)
+        if val is None:
+            val = ''
+        else:
+            val = str(val)
+        return val
+
+    def get_str(self):
+        if self.print_keys:
+            print_keys = self.print_keys
+        else:
+            print_keys = sorted(self.item.keys())
+
+        out = []
+        for ind, key in enumerate(print_keys):
+            out.append(key + ':')
+            if ind < len(print_keys) - 1 or self.tag:
+                ending = ', '
+            else:
+                ending = ''
+
+            val = self.extract(self.item, name)
+            out.append(val + ending)
+        if self.tag:
+            out.append(tag)
+        return ''.join(out)
+
+    def __str__(self):
+        if self._passes_filter():
+            return self.get_str()
+        else:
+            return ''
 
     def __repr__(self):
         return self.__str__()
-
-    def __str__(self):
-        return 'behold'
 
     def __bool__(self):
         return self._passes_filter()
@@ -243,21 +361,41 @@ class in_context(object):
     def __exit__(self, *args, **kwargs):
         self.__class__._behold_class.unset_context(*self._context_vars.keys())
 
+def set_context(**kwargs):
+    Behold.set_context(**kwargs)
+
+def unset_context(cls, *keys):
+    Behold.unset_context(*keys)
+
 
 x, y = 1, 2
-def dummy():
-    a, b = 1, 2
-    dd = {'c': 1, 'd': 2}
-    class Rob(object):
-        def __init__(self):
-            self.e = 1
-            self.f = 2
-    r = Rob()
-    behold = Behold(r)
-    print
-    from pprint import pprint
-    pprint(behold.item)
-dummy()
+
+for nn in range(5):
+    behold = Behold()
+    behold.values('nn', 'x', 'y').when_all(nn=2)
+    if behold:
+        print behold
+
+
+
+
+#set_context(rob='awesome')
+
+#
+#x, y = 1, 2
+#def dummy():
+#    a, b = 1, 2
+#    dd = {'c': 1, 'd': 2}
+#    class Rob(object):
+#        def __init__(self):
+#            self.e = 1
+#            self.f = 2
+#    r = Rob()
+#    behold = Behold(r)
+#    print
+#    from pprint import pprint
+#    pprint(behold.item)
+#dummy()
 
 #@in_context(what='decorator')
 #def myfunc():
